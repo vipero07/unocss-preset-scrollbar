29dda3afeec7a995c393006fb4ec53ec29db2c30
Ou <orongxing@gmail.com>
2024-10-01 22:33:32 +0800

    fix: preset type

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,61 @@
-import type { Preset } from 'unocss'
+import { definePreset } from 'unocss'
 import { colorResolver } from '@unocss/preset-mini/utils';
 
-export function presetScrollbar(): Preset {
-	return {
-		name: 'unocss-preset-scrollbar',
-		variants: [
-			{
-				match: (matcher) => {
-					const matches = matcher.match(
-						/^(resizer|scrollbar(?:-(?:thumb|track(?:-piece)?|button|corner))?):/
-					);
-					if (matches) {
-						return {
-							matcher: matcher.slice(matches[0].length),
-							selector: (s) => `${s}::-webkit-${matches[1]}`
-						};
-					}
-				},
-				multiPass: true,
-			}
+export const presetScrollbar = definePreset({
+	name: 'unocss-preset-scrollbar',
+	rules: [
+		[
+			/^scrollbar-gutter-both$/,
+			() => ({
+				'scrollbar-gutter': 'stable both-edges',
+			})
 		],
-		rules: [
-			[
-				/^scrollbar-gutter-both$/,
-				() => ({
-					'scrollbar-gutter': 'stable both-edges',
-				})
-			],
-			[
-				/^scrollbar-gutter-(auto|stable|inherit|initial|revert|revert-layer|unset)$/,
-				([, p]) => ({
-					'scrollbar-gutter': p,
-				})
-			],
-			[
-				/^scroll(?:bar)?-(thin|none|auto)$/,
-				([, w]) => ({
-					'scrollbar-width': w
-				})
-			],
-			[
-				/^scroll(?:bar)?-(track|thumb)-(.+)$/,
-				([s, section, colorMatch], context) => {
-					const varName = `scroll${section}-bg`;
-					const opacityVarName = `--un-${varName}-opacity`;
-					const colorVarName = `--un-${varName}`;
-					const res = colorResolver('color', varName)([s, colorMatch], context);
+		[
+			/^scrollbar-gutter-(auto|stable|inherit|initial|revert|revert-layer|unset)$/,
+			([, p]) => ({
+				'scrollbar-gutter': p,
+			})
+		],
+		[
+			/^scroll(?:bar)?-(thin|none|auto)$/,
+			([, w]) => ({
+				'scrollbar-width': w
+			})
+		],
+		[
+			/^scroll(?:bar)?-(track|thumb)-(.+)$/,
+			([s, section, colorMatch], context) => {
+				const varName = `scroll${section}-bg`;
+				const opacityVarName = `--un-${varName}-opacity`;
+				const colorVarName = `--un-${varName}`;
+				const res = colorResolver('color', varName)([s, colorMatch], context);
 
-					if (!res) {
-						return;
-					}
+				if (!res) {
+					return;
+				}
 
-					const color = res['color'];
-					const opacity = res[opacityVarName];
+				const color = res['color'];
+				const opacity = res[opacityVarName];
 
-					if (!color) {
-						return;
-					}
+				if (!color) {
+					return;
+				}
 
-					if (opacity) {
-						return {
-							[opacityVarName]: opacity,
-							[colorVarName]: color,
-							'scrollbar-color': 'var(--un-scrollthumb-bg) var(--un-scrolltrack-bg)'
-						};
-					}
-
+				if (opacity) {
 					return {
+						[opacityVarName]: opacity,
 						[colorVarName]: color,
 						'scrollbar-color': 'var(--un-scrollthumb-bg) var(--un-scrolltrack-bg)'
 					};
 				}
-			]
+
+				return {
+					[colorVarName]: color,
+					'scrollbar-color': 'var(--un-scrollthumb-bg) var(--un-scrolltrack-bg)'
+				};
+			}
 		]
-	};
-}
+	]
+})
+
 export default presetScrollbar;
